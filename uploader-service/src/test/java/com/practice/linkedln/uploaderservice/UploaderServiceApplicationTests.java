43a98c8a174f6ce761f71ea3935e5b15ec2f6c6d
Harshit <harshittiwari771@gmail.com>
2025-04-28 23:12:37 +0530

    Creating uploader service

---
+++ uploader-service/src/test/java/com/practice/linkedln/uploaderservice/UploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.linkedln.uploaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
