d8b2e317ad86ea70b9d7a20740cd7980a1dbf053
Harshit <harshittiwari771@gmail.com>
2025-04-29 23:35:38 +0530

    Image uploading using google cloud storage

--- uploader-service/src/main/java/com/practice/linkedln/uploaderservice/service/impl/CloudinaryUploaderService.java
+++ uploader-service/src/main/java/com/practice/linkedln/uploaderservice/service/impl/CloudinaryUploaderService.java
@@ -3,7 +3,6 @@ package com.practice.linkedln.uploaderservice.service.impl;
 import java.io.IOException;
 import java.util.Map;
 
-import javax.management.RuntimeErrorException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
